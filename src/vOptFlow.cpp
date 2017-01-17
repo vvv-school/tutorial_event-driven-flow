a717d11d408c51b334ea8583e0f520c0cc50132f
arrenglover <arren.glover@gmail.com>
2017-01-17 12:09:36 +0100

    cleaned module

--- src/vOptFlow.cpp
+++ src/vOptFlow.cpp
@@ -20,140 +20,31 @@ using yarp::math::operator *;
 using yarp::math::outerProduct;
 
 /******************************************************************************/
-//vFlowModule
+//main
 /******************************************************************************/
-bool vFlowModule::configure(yarp::os::ResourceFinder &rf)
+
+int main(int argc, char * argv[])
 {
-    /* set the name of the module */
-    std::string moduleName = rf.check("name",
-                                      yarp::os::Value("vFlow")).asString();
-    yarp::os::RFModule::setName(moduleName.c_str());
+    /* initialize yarp network */
+    yarp::os::Network yarp;
 
-    bool strict = rf.check("strict") &&
-            rf.check("strict", yarp::os::Value(true)).asBool();
+    /* prepare and configure the resource finder */
+    yarp::os::ResourceFinder rf;
+    rf.setDefaultConfigFile("opticalflow.ini");
+    rf.setDefaultContext("eventdriven");
+    rf.configure(argc, argv);
 
-    /* set parameters */
-    int height = rf.check("height", yarp::os::Value(128)).asInt();
-    int width = rf.check("width", yarp::os::Value(128)).asInt();
-    int sobelSize = rf.check("filterSize", yarp::os::Value(3)).asInt();
-    int minEvtsOnPlane = rf.check("minEvtsThresh", yarp::os::Value(5)).asInt();
-
-    flowmanager = new vFlowManager(height, width, sobelSize, minEvtsOnPlane);
-    return flowmanager->open(moduleName, strict);
-
-}
-
-/******************************************************************************/
-bool vFlowModule::interruptModule()
-{
-    flowmanager->interrupt();
-    yarp::os::RFModule::interruptModule();
-
-    return true;
-}
-
-/******************************************************************************/
-bool vFlowModule::close()
-{
-    flowmanager->close();
-    delete flowmanager;
-    yarp::os::RFModule::close();
-
-    return true;
-}
-
-/******************************************************************************/
-double vFlowModule::getPeriod() {
-    return 1.0;
-}
-
-/******************************************************************************/
-bool vFlowModule::updateModule()
-{
-    return true;
+    /* instantiate the module */
+    vFlowModule module;
+    return module.runModule(rf);
 }
 
 /******************************************************************************/
 //vFlowManager
 /******************************************************************************/
-vFlowManager::vFlowManager(int height, int width, int filterSize,
-                                     int minEvtsOnPlane)
-{
 
-    this->height=height;
-    this->width=width;
-
-    //ensure sobel size is at least 3 and an odd number
-    if(filterSize < 5) filterSize = 3;
-    if(!(filterSize % 2)) filterSize--;
-    this->fRad = filterSize / 2;
-    this->halfCount = pow(filterSize, 2.0) / 2.0 + 0.5;
-    this->planeSize = pow(filterSize, 2.0);
-
-    this->minEvtsOnPlane = minEvtsOnPlane;
-
-    //for speed we predefine the mememory for some matricies
-    At = yarp::sig::Matrix(3, filterSize * filterSize);
-    AtA = yarp::sig::Matrix(3,3);
-    abc = yarp::sig::Vector(3);
-    A2 = yarp::sig::Matrix(3, 3);
-
-    eventsComputed = 0;
-    eventsPotential = 0;
-    bottleCount = 0;
-
-    //create our surface in synchronous mode
-    surfaceOnL = new eventdriven::temporalSurface(width, height);
-    surfaceOfL = new eventdriven::temporalSurface(width, height);
-    surfaceOnR = new eventdriven::temporalSurface(width, height);
-    surfaceOfR = new eventdriven::temporalSurface(width, height);
-
-}
-
-/******************************************************************************/
-bool vFlowManager::open(std::string moduleName, bool strictness)
-{
-    this->strictness = strictness;
-    if(strictness) {
-        std::cout << "Setting " << moduleName << " to strict" << std::endl;
-        this->setStrict();
-    }
-    this->useCallback();
-
-    std::string inPortName = "/" + moduleName + "/vBottle:i";
-    bool check1 = BufferedPort<eventdriven::vBottle>::open(inPortName);
-
-    if(strictness) outPort.setStrict();
-    std::string outPortName = "/" + moduleName + "/vBottle:o";
-    bool check2 = outPort.open(outPortName);
-    return check1 && check2;
-}
-
-/******************************************************************************/
-void vFlowManager::close()
-{
-    /*close ports*/
-    outPort.close();
-    yarp::os::BufferedPort<eventdriven::vBottle>::close();
-
-    delete surfaceOnL;
-    delete surfaceOfL;
-    delete surfaceOnR;
-    delete surfaceOfR;
-
-}
-
-/******************************************************************************/
-void vFlowManager::interrupt()
-{
-    outPort.interrupt();
-    yarp::os::BufferedPort<eventdriven::vBottle>::interrupt();
-}
-
-/******************************************************************************/
 void vFlowManager::onRead(eventdriven::vBottle &inBottle)
 {
-    //bool computeflow = true;
 
     /*prepare output vBottle with AEs extended with optical flow events*/
     eventdriven::vBottle &outBottle = outPort.prepare();
@@ -168,8 +59,9 @@ void vFlowManager::onRead(eventdriven::vBottle &inBottle)
     {
         eventdriven::AddressEvent *aep = (*qi)->getAs<eventdriven::AddressEvent>();
         if(!aep) continue;
-        //if(aep->getChannel()) continue;
 
+        //add the event to the appropriate surface
+        eventdriven::vSurface2 * cSurf;
         if(aep->getChannel()) {
             if(aep->getPolarity())
                 cSurf = surfaceOfR;
@@ -182,45 +74,99 @@ void vFlowManager::onRead(eventdriven::vBottle &inBottle)
                 cSurf = surfaceOnL;
         }
 
+        //compute the flow
         cSurf->addEvent(*aep);
-
-        eventdriven::FlowEvent *ofe = compute();
-//        eventsComputed++;
-//        eventsPotential++;
-
-        if(ofe) outBottle.addEvent(*ofe);
-        else outBottle.addEvent(*aep);
+        double vx, vy;
+        if(compute(cSurf, vx, vy)) {
+            //successfully computed a flow event
+            eventdriven::FlowEvent vf(*aep);
+            vf.setVx(vx);
+            vf.setVy(vy);
+            outBottle.addEvent(vf);
+        } else {
+            outBottle.addEvent(*aep);
+        }
 
     }
 
-//    bottleCount++;
-
-//    if(bottleCount > 2000) {
-//        std::cout << (int)(eventsComputed *100.0 / eventsPotential);
-//        std::cout << "% (" << eventsComputed << ")" << std::endl;
-//        eventsComputed = 0; eventsPotential = 0; bottleCount = 0;
-//    }
-
-
     if(strictness) outPort.writeStrict();
     else outPort.write();
+}
+
+vFlowManager::vFlowManager(int height, int width, int filterSize,
+                                     int minEvtsOnPlane)
+{
+    //ensure sobel size is at least 3 and an odd number
+    if(filterSize < 5) filterSize = 3;
+    if(!(filterSize % 2)) filterSize--;
+    this->fRad = filterSize / 2;
+    this->planeSize = pow(filterSize, 2.0);
+
+    this->minEvtsOnPlane = minEvtsOnPlane;
+
+    //for speed we predefine the mememory for some matricies
+    At = yarp::sig::Matrix(3, filterSize * filterSize);
+    AtA = yarp::sig::Matrix(3,3);
+    abc = yarp::sig::Vector(3);
+    A2 = yarp::sig::Matrix(3, 3);
+
+
+    //create our surface in synchronous mode
+    surfaceOnL = new eventdriven::temporalSurface(width, height);
+    surfaceOfL = new eventdriven::temporalSurface(width, height);
+    surfaceOnR = new eventdriven::temporalSurface(width, height);
+    surfaceOfR = new eventdriven::temporalSurface(width, height);
+}
+
+bool vFlowManager::open(std::string moduleName, bool strictness)
+{
+    //set strict mode if necessary
+    this->strictness = strictness;
+    if(strictness) {
+        std::cout << "Setting " << moduleName << " to strict" << std::endl;
+        this->setStrict();
+    }
+
+    //open the input port
+    this->useCallback(); //we need callback to use the onRead() function
+    if(!yarp::os::BufferedPort<eventdriven::vBottle>::open("/" + moduleName + "/vBottle:i"))
+        return false;
+
+    //open the output port
+    if(!outPort.open("/" + moduleName + "/vBottle:o"))
+        return false;
+
+    return true;
+}
+
+void vFlowManager::close()
+{
+    /*close ports*/
+    outPort.close();
+    yarp::os::BufferedPort<eventdriven::vBottle>::close();
+
+    delete surfaceOnL;
+    delete surfaceOfL;
+    delete surfaceOnR;
+    delete surfaceOfR;
 
 }
 
-/******************************************************************************/
-#define COS135on2 0.38268
-eventdriven::FlowEvent *vFlowManager::compute()
+void vFlowManager::interrupt()
 {
-    eventdriven::FlowEvent * vf = NULL;
-    double dtdy = 0, dtdx = 0;
+    outPort.interrupt();
+    yarp::os::BufferedPort<eventdriven::vBottle>::interrupt();
+}
+
+bool vFlowManager::compute(eventdriven::vSurface2 *surf, double &vx, double &vy)
+{
 
     //get the most recent event
     eventdriven::AddressEvent * vr =
-            cSurf->getMostRecent()->getAs<eventdriven::AddressEvent>();
-
+            surf->getMostRecent()->getAs<eventdriven::AddressEvent>();
 
     //find the side of this event that has the collection of temporally nearby
-    //events
+    //events. Heuristically more likely to be the correct plane.
     double bestscore = eventdriven::vtsHelper::maxStamp()+1;
     int besti = 0, bestj = 0;
 
@@ -228,7 +174,7 @@ eventdriven::FlowEvent *vFlowManager::compute()
         for(int j = vr->getY()-fRad; j <= vr->getY()+fRad; j+=fRad) {
             //get the surface around the recent event
             double sobeltsdiff = 0;
-            const eventdriven::vQueue &subsurf = cSurf->getSurf(i, j, fRad);
+            const eventdriven::vQueue &subsurf = surf->getSurf(i, j, fRad);
             if(subsurf.size() < planeSize) continue;
 
             for(unsigned int k = 0; k < subsurf.size(); k++) {
@@ -246,67 +192,19 @@ eventdriven::FlowEvent *vFlowManager::compute()
             }
         }
     }
+    //return if we don't find a good candidate plane
+    if(bestscore > eventdriven::vtsHelper::maxStamp()) return false;
 
-    if(bestscore > eventdriven::vtsHelper::maxStamp()) return vf;
+    //get the events
+    const eventdriven::vQueue &subsurf = surf->getSurf(besti, bestj, fRad);
 
-    const eventdriven::vQueue &subsurf = cSurf->getSurf(besti, bestj, fRad);
+    //and compute the gradients of the plane
+    if(computeGrads(subsurf, *vr, vy, vx) < minEvtsOnPlane)
+        return false;
 
-    //and compute the flow
-    if(computeGrads(subsurf, *vr, dtdy, dtdx) >= minEvtsOnPlane) {
-        vf = new eventdriven::FlowEvent(*vr);
-        vf->setVx(dtdx);
-        vf->setVy(dtdy);
-        vf->setDeath();
-    }
-//    if(!vf) {
-//        //the continuation
-
-//        double adx = 0, ady = 0;
-//        int an = 0;
-//        eventdriven::vQueue::const_iterator qi;
-//        for(qi = subsurf.begin(); qi != subsurf.end(); qi++) {
-
-//            eventdriven::FlowEvent *vf2 = (*qi)->getAs<eventdriven::FlowEvent>();
-//            if(!vf2) continue;
-
-////            adx += vf2->getVx();
-////            ady += vf2->getVy();
-////            an++;
-
-//            double vx = vf2->getVy(); double vy = vf2->getVx();
-//            double mag = sqrt(pow(vx, 2.0) + pow(vy, 2.0));
-//            vx /= (mag * COS135on2);
-//            vy /= (mag * COS135on2);
-//            int dx = 0, dy = 0;
-//            if(vx > 1) dx = 1; if(vx < -1) dx = -1;
-//            if(vy > 1) dy = 1; if(vy < -1) dy = -1;
-
-//            if(vf2->getX() + dx == vr->getX() && vf2->getY() - dy == vr->getY()
-//                    || vf2->getX() - dx == vr->getX() && vf2->getY() + dy == vr->getY()
-//                    || vf2->getX() + dx == vr->getX() && vf2->getY() + dy == vr->getY()
-//                    ) {
-//                adx += vf2->getVx();
-//                ady += vf2->getVy();
-//                an++;
-//            }
-//        }
-
-//        if(an > 1) {
-//            vf = new eventdriven::FlowEvent(*vr);
-//            vf->setVx(adx / an);
-//            vf->setVy(ady / an);
-//            vf->setDeath();
-//            std::cout << "Upgraded event from " << an << " others" << std::endl;
-//        }
-
-//    }
-
-    if(vf) cSurf->addEvent(*vf);
-
-    return vf;
+    return true;
 }
 
-/******************************************************************************/
 int vFlowManager::computeGrads(const eventdriven::vQueue &subsurf,
                                      eventdriven::AddressEvent &cen,
                                      double &dtdy, double &dtdx)
@@ -330,7 +228,6 @@ int vFlowManager::computeGrads(const eventdriven::vQueue &subsurf,
                         eventdriven::vtsHelper::tstosecs(), dtdy, dtdx);
 }
 
-/******************************************************************************/
 int vFlowManager::computeGrads(yarp::sig::Matrix &A, yarp::sig::Vector &Y,
                                      double cx, double cy, double cz,
                                      double &dtdy, double &dtdx)
@@ -382,6 +279,55 @@ int vFlowManager::computeGrads(yarp::sig::Matrix &A, yarp::sig::Vector &Y,
 
 
     return inliers;
+}
 
+/******************************************************************************/
+//vFlowModule
+/******************************************************************************/
+bool vFlowModule::configure(yarp::os::ResourceFinder &rf)
+{
+    /* set the name of the module */
+    std::string moduleName = rf.check("name",
+                                      yarp::os::Value("vFlow")).asString();
+    yarp::os::RFModule::setName(moduleName.c_str());
+
+    bool strict = rf.check("strict") &&
+            rf.check("strict", yarp::os::Value(true)).asBool();
+
+    /* set parameters */
+    int height = rf.check("height", yarp::os::Value(128)).asInt();
+    int width = rf.check("width", yarp::os::Value(128)).asInt();
+    int sobelSize = rf.check("filterSize", yarp::os::Value(3)).asInt();
+    int minEvtsOnPlane = rf.check("minEvtsThresh", yarp::os::Value(5)).asInt();
+
+    flowmanager = new vFlowManager(height, width, sobelSize, minEvtsOnPlane);
+    return flowmanager->open(moduleName, strict);
 
 }
+
+bool vFlowModule::interruptModule()
+{
+    flowmanager->interrupt();
+    yarp::os::RFModule::interruptModule();
+
+    return true;
+}
+
+bool vFlowModule::close()
+{
+    flowmanager->close();
+    delete flowmanager;
+    yarp::os::RFModule::close();
+
+    return true;
+}
+
+double vFlowModule::getPeriod() {
+    return 1.0;
+}
+
+bool vFlowModule::updateModule()
+{
+    return true;
+}
+
